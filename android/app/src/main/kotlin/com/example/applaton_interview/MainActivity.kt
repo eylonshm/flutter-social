1d7cd5e89370fa7a1d8ee937a47e26cdc1c904c7
Eylon Shmilovich <eylonshm@gmail.com>
2021-07-18 16:30:49 +0300

    First initialization

---
+++ android/app/src/main/kotlin/com/example/applaton_interview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.applaton_interview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(myApp());
+
+class myApp extends StatelessWidget {}
